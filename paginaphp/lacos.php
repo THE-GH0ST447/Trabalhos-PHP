433c63c339bcdb2741b822910530636b773d3473
João pedro S <136065428+THE-GH0ST447@users.noreply.github.com>
2023-10-25 14:38:37 -0300

    Add files via upload

---
+++ paginaphp/lacos.php
@@ -0,0 +1,22 @@
+<?php
+//WHILE
+$numero = 0;
+while ($numero < 1) {
+    echo $numero;
+    echo ("$numero <br>");
+    $numero = $numero + 1;
+    echo (" $numero++ ");
+}
+ 
+ 
+echo "<br>";
+switch ($variavel = "Mulher") {
+    case "homem";
+        echo "Bem vindo ao exercito";
+        break;
+    case "homem";
+    default;
+        echo "SELECIONE ALGUMA OPÇÃO";
+        case "homem";
+        echo "Bem vindo ao exercito";
+}
